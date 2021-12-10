7bfa2c395b54cc746d6723ff3fd1677c3c0cb768
tcdude <tc@tizilogic.com>
2021-12-10 22:31:25 +0100

    expose set_font_size

--- Sources/krink/graphics2/textpainter.h
+++ Sources/krink/graphics2/textpainter.h
@@ -15,6 +15,7 @@ void krink_g2_tsp_draw_buffer(bool end);
 void krink_g2_tsp_set_bilinear_filter(bool bilinear);
 void krink_g2_tsp_set_projection_matrix(kinc_matrix4x4_t mat);
 void krink_g2_tsp_set_font(krink_ttf_font_t *font);
+void krink_g2_tsp_set_font_size(int size);
 void krink_g2_tsp_draw_string(const char *text, float opacity, uint32_t color, float x, float y,
                               kinc_matrix3x3_t transformation);
 void krink_g2_tsp_draw_characters(int *text, int start, int length, float opacity, uint32_t color,
